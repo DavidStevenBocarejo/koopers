76b0d0e9439180aeb4504394bde24c80badf1c95
David Steven Bocarejo Ibañez <david.negocios1979@gmail.com>
2020-10-03 21:21:01 -0500

    Test fallidos

--- api-rest/apps/api/tests.py
+++ api-rest/apps/api/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+import unittest
+
+#Models
+#from ..core.models import *
+#from .models import *
+#from api-rest.apps.core.models import Category
+#from ..apps.core.models import Category
+#from apps.core.models import Category
 
 # Create your tests here.
+class TestStringMethods(unittest.TestCase):
+
+    def setUp(self):
+        Category.objects.create(
+            title='Test de prueva',
+        )
+    
+    def Test_category_exist(self):
+        test_categry = Category.objects.filter(title='Test de prueva').exist()
+        self.assertEqual(test_categry, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- api-rest/koopers/settings.py
+++ api-rest/koopers/settings.py
@@ -109,13 +109,13 @@ WSGI_APPLICATION = 'koopers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -126,7 +126,7 @@ DATABASES = {
         'PORT': env("DATABASE_PORT"),
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
